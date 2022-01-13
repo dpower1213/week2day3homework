dd4777b5456449b2a84a6205a108477553b67109
dpower1213 <power4u@outlook.com>
2022-01-13 15:29:42 -0800

    first commit

---
+++ area_mod.py
@@ -0,0 +1,12 @@
+# import math
+# from math import pi
+
+def area(length,width):
+    area=length*width
+    return area
+    
+
+def areacircle(radius):
+    areacircle=2*radius*pi
+    return areacircle
+    
